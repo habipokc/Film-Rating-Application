a94a122fd590c6515d2f95057b27f322b719752c
Habip <114939978+habipokc@users.noreply.github.com>
2023-05-10 06:22:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from wtforms.validators import DataRequired
 import requests
 
 
-MOVIE_DB_API_KEY = "346ffeb7ad10cefb98831bba28043a91"
+MOVIE_DB_API_KEY = "YOUR-API-KEY"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
